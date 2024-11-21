7f7462417e6098c61b78338a9428bf95f18ef278
Artem Kozynets <kozineca10@gmail.com>
2024-11-21 19:46:25 +0100

    New coomit -1

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-import * as THREE from 'three';
-import '@google/model-viewer';
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPopup = document.querySelector('.btnLogin-popup')
+const iconClose = document.querySelector('.icon-close')
 
-const container = document.getElementById('container');
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+} );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement);
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+} );
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1).normalize();
-scene.add(light);
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+} );
 
-const loader = new THREE.GLTFLoader();
-loader.load('model.glb', function (gltf) {
-    scene.add(gltf.scene);
-    gltf.scene.rotation.y = Math.PI;
-});
-
-camera.position.z = 5;
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
-
-function openPage() {
-    window.location.href = "login.html";
-}
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+} );
