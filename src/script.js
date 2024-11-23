35c0d2eaa7d3ba756e5125f0d1e9ff8216d3d884
Artem Kozynets <kozineca10@gmail.com>
2024-11-23 13:41:03 +0100

    23.11

--- src/script.js
+++ src/script.js
@@ -1,25 +1,8 @@
-const wrapper = document.querySelector('.wrapper')
-const loginLink = document.querySelector('.login-link')
-const registerLink = document.querySelector('.register-link')
-const btnPopup = document.querySelector('.btnLogin-popup')
-const iconClose = document.querySelector('.icon-close')
+const btnDown = document.getElementById('btnDown');
 
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-} );
-
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-} );
-
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-} );
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-} );
-
-function openPage() {
-    window.location.href = "login.html";
-}
+btnDown.addEventListener('click', () => {
+    window.scrollBy({
+        top: window.innerHeight,
+        behavior: 'smooth',
+    });
+});
