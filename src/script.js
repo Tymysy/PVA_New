548e85451c6b51581b79f605c6de632b1894c6cb
Artem Kozynets <kozineca10@gmail.com>
2024-11-24 18:57:00 +0100

    24.11

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@ btnDown.addEventListener('click', () => {
         behavior: 'smooth',
     });
 });
+
+function openM4() {
+    window.location.href = "./carsHtml/M4.html"
+}
+
+function openX3() {
+    window.location.href = "./carsHtml/X3.html"
+}
+
+function openX5() {
+    window.location.href = "./carsHtml/X5.html"
+}
+
+function open7S() {
+    window.location.href = "./carsHtml/7S.html"
+}
